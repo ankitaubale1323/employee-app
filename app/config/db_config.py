acae9d35058d82f66e46ae5d3252b22f6fe1929a
ankitaubale1323 <ankitaaubale1323@gmail.com>
2026-03-12 09:58:12 +0530

    db config updated

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,36 +1,4 @@
-# import mysql.connector
-# import os
-# import time
 
-# def get_db_connection():
-#     for i in range(10):
-#         try:
-#             conn = mysql.connector.connect(
-#                 host=os.getenv("DB_HOST", "mysql"),
-#                 user=os.getenv("DB_USER", "employee_user"),
-#                 password=os.getenv("DB_PASSWORD", "root"),
-#                 database=os.getenv("DB_NAME", "employee_db"),
-#                 port=int(os.getenv("DB_PORT", 3306))
-#             )
-
-#             # Auto-create employees table if it doesn't exist
-#             cursor = conn.cursor()
-#             cursor.execute("""
-#                 CREATE TABLE IF NOT EXISTS employees (
-#                     id INT AUTO_INCREMENT PRIMARY KEY,
-#                     name VARCHAR(100) NOT NULL,
-#                     role VARCHAR(100) NOT NULL,
-#                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-#                 )
-#             """)
-#             conn.commit()
-
-#             return conn
-#         except Exception as e:
-#             print("Waiting for MySQL...", e)
-#             time.sleep(3)
-
-#     raise Exception("Could not connect to MySQL")
 
 import mysql.connector
 import os
@@ -40,11 +8,11 @@ def get_db_connection():
     for i in range(10):
         try:
             conn = mysql.connector.connect(
-                host=os.getenv("MYSQL_HOST", "mysql"),
-                user=os.getenv("MYSQL_USER", "flaskuser"),
-                password=os.getenv("MYSQL_PASSWORD", "securepass"),
-                database=os.getenv("MYSQL_DATABASE", "flask_db"),
-                port=int(os.getenv("MYSQL_PORT", 3306))
+    host=os.getenv("MYSQL_HOST", "employee-mysql"),
+    user=os.getenv("MYSQL_USER", "flaskuser"),
+    password=os.getenv("MYSQL_PASSWORD", "securepass"),
+    database=os.getenv("MYSQL_DATABASE", "employee_db"),
+    port=int(os.getenv("MYSQL_PORT", 3306))
             )
 
             # Auto-create employees table if it doesn't exist
